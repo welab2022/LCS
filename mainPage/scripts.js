722f0cae16ad8210e957fc1b522d90aa61f88a84
HoangHung <conbodu68@gmail.com>
2022-06-03 09:47:03 +0700

    add mainpage

---
+++ mainPage/scripts.js
@@ -0,0 +1,15 @@
+
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Toggle the side navigation
+    const sidebarToggle = document.body.querySelector('#sidebarToggle');
+    if (sidebarToggle) {
+       
+        sidebarToggle.addEventListener('click', event => {
+            event.preventDefault();
+            document.body.classList.toggle('sb-sidenav-toggled');
+        });
+    }
+
+});
